22c5127108975e71aa9a85f34c1d0afa1e761592
Ali Mohamed <alimohamed351@outlook.com>
2026-04-14 17:43:54 +0200

    added zod validation

--- src/utils/error/index.ts
+++ src/utils/error/index.ts
@@ -2,31 +2,32 @@ export class AppError extends Error {
   constructor(
     message: string,
     public statusCode: number,
+    public errorDetails?: Record<string, any>[], // key, value,
   ) {
     super(message);
   }
 }
 
 export class ConflictException extends AppError {
-  constructor(message: string) {
-    super(message, 409);
+  constructor(message: string, errorDetails?: Record<string, string>[]) {
+    super(message, 409, errorDetails);
   }
 }
 
 export class NotFoundException extends AppError {
-  constructor(message: string) {
-    super(message, 404);
+  constructor(message: string, errorDetails?: Record<string, string>[]) {
+    super(message, 404, errorDetails);
   }
 }
 
 export class NotAuthorizedException extends AppError {
-  constructor(message: string) {
-    super(message, 401);
+  constructor(message: string, errorDetails?: Record<string, string>[]) {
+    super(message, 401, errorDetails);
   }
 }
 
 export class BadRequestException extends AppError {
-  constructor(message: string) {
-    super(message, 400);
+  constructor(message: string, errorDetails?: Record<string, string>[]) {
+    super(message, 400, errorDetails);
   }
 }
